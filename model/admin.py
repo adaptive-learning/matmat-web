71a6bad6c14c9c0fb3c1ed6288f37534cb116281
thran <pkg7fp13>
2014-04-09 10:34:06 +0200

    skill added to administration

--- model/admin.py
+++ model/admin.py
@@ -1,3 +1,8 @@
 from django.contrib import admin
+from model.models import Skill
 
-# Register your models here.
+class SkillManager(admin.ModelAdmin):
+    list_display = ('name', 'parent', 'level', 'note')
+
+
+admin.site.register(Skill, SkillManager)
--- model/models.py
+++ model/models.py
@@ -4,4 +4,8 @@ from django.db import models
 class Skill(models.Model):
     name = models.CharField(max_length=30)
     note = models.TextField(blank=True, null=True)
-    parent = models.ForeignKey("model.Skill", null=True)
+    parent = models.ForeignKey("model.Skill", null=True, blank=True)
+    level = models.IntegerField()
+
+    def __unicode__(self):
+        return self.name
