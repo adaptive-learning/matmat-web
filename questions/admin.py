4577f889fabad9c302cc524db5c4456ebe765b89
thran <pkg7fp13>
2014-04-09 10:49:48 +0200

    questions model to administration

--- questions/admin.py
+++ questions/admin.py
@@ -1,3 +1,12 @@
 from django.contrib import admin
+from questions.models import Player, Question
 
-# Register your models here.
+
+class PlayerManager(admin.ModelAdmin):
+    list_display = ('name', 'note')
+
+class QuestionManager(admin.ModelAdmin):
+    list_display = ('player', 'skill', 'data')
+
+admin.site.register(Player, PlayerManager)
+admin.site.register(Question, QuestionManager)
