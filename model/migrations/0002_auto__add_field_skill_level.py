4585a45560cdc153d2e8d575257d06634ccae087
thran <pkg7fp13>
2014-04-09 10:32:42 +0200

    skill level added to model

---
+++ model/migrations/0002_auto__add_field_skill_level.py
@@ -0,0 +1,33 @@
+# -*- coding: utf-8 -*-
+from south.utils import datetime_utils as datetime
+from south.db import db
+from south.v2 import SchemaMigration
+from django.db import models
+
+
+class Migration(SchemaMigration):
+
+    def forwards(self, orm):
+        # Adding field 'Skill.level'
+        db.add_column(u'model_skill', 'level',
+                      self.gf('django.db.models.fields.IntegerField')(default=0),
+                      keep_default=False)
+
+
+    def backwards(self, orm):
+        # Deleting field 'Skill.level'
+        db.delete_column(u'model_skill', 'level')
+
+
+    models = {
+        u'model.skill': {
+            'Meta': {'object_name': 'Skill'},
+            u'id': ('django.db.models.fields.AutoField', [], {'primary_key': 'True'}),
+            'level': ('django.db.models.fields.IntegerField', [], {}),
+            'name': ('django.db.models.fields.CharField', [], {'max_length': '30'}),
+            'note': ('django.db.models.fields.TextField', [], {'null': 'True', 'blank': 'True'}),
+            'parent': ('django.db.models.fields.related.ForeignKey', [], {'to': u"orm['model.Skill']", 'null': 'True'})
+        }
+    }
+
+    complete_apps = ['model']
