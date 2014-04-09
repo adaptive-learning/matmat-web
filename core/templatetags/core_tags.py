e49db754bb86ee3e0f8aa3b6ad3a131b219fe00a
Thran <exthran@gmail.com>
2014-04-09 22:47:09 +0200

    oh - fix of so stupid bug

--- core/templatetags/core_tags.py
+++ core/templatetags/core_tags.py
@@ -5,7 +5,7 @@ register = template.Library()
 @register.filter(name='is_auth')
 def is_auth(value):
 
-    if not value.is_authenticated or value.is_anonymous:
+    if not value.is_authenticated or value.is_anonymous():
         return False
 
     return value.social_auth.exists()
