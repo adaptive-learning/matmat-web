877a03a343e4dbcd7fdcd1a7bec0bf6606eb167c
Thran <exthran@gmail.com>
2014-04-18 10:20:55 +0200

    questions recommendation optimalization

--- model/recommendation.py
+++ model/recommendation.py
@@ -9,7 +9,7 @@ def recommend_questions(user, skills):
         "COUNT(questions_answer.id) AS answers_count, "
         "MIN(TIME_TO_SEC(TIMEDIFF(NOW(), questions_answer.timestamp))) AS time_form_last_answer,"
         "model_userskill.value as user_skill, "
-        "model_questiondifficulty.* "
+        "model_questiondifficulty.value as questiondifficulty "
         "FROM questions_question "
         "LEFT JOIN model_userskill ON ( model_userskill.skill_id = questions_question.skill_id AND model_userskill.user_id = %(user_id)s)"
         "LEFT JOIN questions_answer ON ( questions_question.id = questions_answer.question_id AND questions_answer.user_id = %(user_id)s) "
@@ -36,7 +36,7 @@ def question_priority(question):
     time_score = -1 / (question.time_form_last_answer) if question.time_form_last_answer > 0 else -1
 
     user_skill = question.user_skill if question.user_skill is not None else 0 # TODO predelat
-    expected_response = get_expected_response(user_skill, question.difficulty.value, question.type)
+    expected_response = get_expected_response(user_skill, question.questiondifficulty, question.type)
     if GOAL_RESPONSE > expected_response:
         estimate_score = expected_response / GOAL_RESPONSE
     else:
