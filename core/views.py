7fb190bd777ace0511276054cd341769843ab2b0
Thran <exthran@gmail.com>
2014-04-18 12:07:48 +0200

    remember user

--- core/views.py
+++ core/views.py
@@ -1,3 +1,5 @@
+from django.contrib.auth import user_logged_in
+from django.dispatch import receiver
 from django.shortcuts import render
 from lazysignup.decorators import allow_lazy_user
 from model.models import Skill
@@ -8,3 +10,7 @@ def home(request):
     return render(request, 'core/home.html', {
         "skills": Skill.objects.filter(level=2)
     })
+
+@receiver(user_logged_in)
+def remember_user(request, **kwargs):
+    request.session.set_expiry(0)
