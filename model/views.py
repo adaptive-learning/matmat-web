9bb2452fb0927538b5bf2df9e117fd4b4365a19d
Juraj Niznan <jurajniznan@gmail.com>
2014-04-21 00:30:00 +0200

    skillmap - added numbers

--- model/views.py
+++ model/views.py
@@ -6,26 +6,30 @@ from model.models import UserSkill, Skill
 @allow_lazy_user
 def my_skills(request):
     skills = set()
-    for s in Skill.objects.filter(parent__name='addition <= 10'):
-        skills.add(s.name)
-    for s in Skill.objects.filter(parent__name='addition <= 20'):
+    for s in Skill.objects.\
+            filter(parent__name__in=['addition <= 10', 'addition <= 20',
+                                     'numbers10', 'numbers20', 'numbers100']):
         skills.add(s.name)
     user_skills = {k: None for k in skills}
     for us in UserSkill.objects.filter(user=request.user):
         user_skills[us.skill.name] = us.value
 
-    def _get(name):
-        if name in user_skills:
-            return {'name': name, 'style': get_style(user_skills[name])}
-        else:
-            return {'name': '', 'style': get_style(None)}
-
-    rows = [[_get('%s+%s' % (c, r)) for c in range(1, 11)]
-            for r in range(1, 21)]
+    tables = []
+    tables.append([[get_skill_repr(str(c + r * 10), user_skills)
+                    for c in range(1, 11)] for r in range(10)])
+    tables.append([[get_skill_repr('%s+%s' % (c, r), user_skills)
+                    for c in range(1, 11)] for r in range(1, 21)])
 
     return render(request, 'model/my_skills.html', {
-        "rows": rows,
+        "tables": tables,
     })
+
+
+def get_skill_repr(name, user_skills):
+    if name in user_skills:
+        return {'name': name, 'style': get_style(user_skills[name])}
+    else:
+        return {'name': '', 'style': get_style(None)}
 
 
 def get_style(value):
