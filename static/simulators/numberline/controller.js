342f690ff1ef45ec9e6462bb88774efa668ac31d
Thran <exthran@gmail.com>
2014-05-30 12:21:15 +0200

    numberline simulator - more work

--- static/simulators/numberline/controller.js
+++ static/simulators/numberline/controller.js
@@ -12,15 +12,31 @@ app.directive("numberline", function(){
             $scope.selected_number = null;
 
             $scope.settings = {
-                width: 600,
+                width: 800,
                 height: 50,
                 offset: 50,
-                top: 30,
+                top: 15,
                 hover_size: 30
             };
 
+            // pseudo random range
+            var str = "salt"+$scope.data.question;
+            var hash = 0, chr;
+            for (var i = 0; i < str.length; i++ ){
+                chr = str.charCodeAt(i);
+                hash  = ((hash << 5) - hash) + chr;
+                hash |= 0;
+            }
+            hash = Math.abs(hash);
+
+            var bound = Math.ceil($scope.data.answer/10)*10;
+            console.log(hash);
+            console.log($scope.data.answer > 7, 10 * (hash % 2));
+            if (18 > $scope.data.answer > 7) bound += 10 * (hash % 2);
+            if ($scope.data.answer > 17) bound += 10 * (hash % 3);
+
             $scope.points = [];
-            $scope.range = [0, 10];
+            $scope.range = [0, bound];
 
 
             $scope.populate_points = function(){
@@ -34,11 +50,14 @@ app.directive("numberline", function(){
                     point.r = 3;
                     if (point.number % 5 == 0) point.r = 4;
                     if (point.number % 10 == 0) point.r = 5;
+                    point.display = "none";
+                    if (i == $scope.range[0] || i == $scope.range[1]) point.display = "block";
                     $scope.points.push(point);
                 }
             };
 
             $scope.select = function(point){
+                if ($scope.nokShow || $scope.okShow) return;
                 var number = parseInt(point.id.replace("point", ""));
                 point = $(point);
                 $("g").attr("class", $("g").attr("class").replace(" selected", ""));
@@ -51,7 +70,17 @@ app.directive("numberline", function(){
 
                 $scope.$apply();
 
+            };
 
+            $scope.check_answer = function(){
+                var correct = $scope.selected_number == $scope.data.answer;
+                $(".numberline-point text").show();
+                $("#point"+$scope.data.answer+" text").css("font-weight", "bold");
+                $("#point"+$scope.data.answer+" circle.shadow").css("opacity", 1).css("fill", "green");
+                $("#point"+$scope.data.answer+" circle.point").css("fill", "green");
+                $scope.okShow = correct;
+                $scope.nokShow = !correct;
+                $scope.interface.finish(correct, 3000);
             };
 
             $scope.populate_points();
