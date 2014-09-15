d1335506ddb02f15748d5f44ec9bba15db959004
Juraj Niznan <jurajniznan@gmail.com>
2014-09-15 16:07:47 +0200

    forgot to add fillin simulator

---
+++ static/simulators/fillin/controller.js
@@ -0,0 +1,26 @@
+app.directive("fillin", function(){
+    return {
+        restrict: "E",
+        scope: {
+            data: "=data",
+            interface: "=interface"
+        },
+        templateUrl: static_url + "simulators/fillin/simulator.html",
+        controller: function($scope){
+            $scope.fill = '_';
+            $scope.answer = '';
+            $("#simulator-input").focus();
+
+            $scope.check_answer = function(){
+                $scope.answer = $scope.answer.replace(/\s*-\s*/g,'-').trim();  
+                var correct = $scope.answer == $scope.data.answer;
+                $scope.interface.finish(correct);
+            };
+
+            $scope.change = function(){
+                $scope.interface.log($scope.answer);
+                $scope.fill = ($scope.answer == '') ? '__' : $scope.answer;
+            };
+        }
+    }
+});
