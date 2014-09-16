3436f95d11aa2dd4b4434b52630ada404b3f1cf6
Thran <exthran@gmail.com>
2014-09-16 13:57:45 +0200

    update of elo model - now works with differences from ancestors skills

--- elo/model.py
+++ elo/model.py
@@ -16,7 +16,7 @@ class EloModel():
             return -(user_skill - difficulty)
 
     @staticmethod
-    def compute_user_skill_delta(response, expected_response, question_type):
+    def compute_user_skill_delta(response, expected_response, question_type, level):
         K = 0.7
         K_correct = 1
         K_incorrect = 0.5
@@ -30,7 +30,9 @@ class EloModel():
         if question_type == 't':
             delta = K * (expected_response - response)
 
-        return delta
+        level_coef = 1. / 3 ** (4 - level)   # level is in range [1,4]
+
+        return level_coef * delta
 
     @staticmethod
     def compute_difficulty_delta(response, expected_response, first_attempts_count, question_type):
@@ -53,46 +55,59 @@ class EloModel():
             return math.log(self.data.get_solving_time(answer))
 
     def get_user_skill(self, user, skill):
+        """
+        compute absolute and relative skill of user
+        @return: absolute skill, relative skill
+        """
+
+        parent_skill = self.data.get_parent_skill(skill)
         user_skill = self.data.get_user_skill(user, skill)
-        if user_skill is None:
-            # propagate skill form parent if doesnt exist
-            parent_skill = self.data.get_parent_skill(skill)
-            if parent_skill is None:
-                user_skill = INITIAL_SKILL
+
+        # stop recursion on root
+        if parent_skill is None:
+            if user_skill is None:
+                return INITIAL_SKILL, INITIAL_SKILL
             else:
-                user_skill = self.get_user_skill(user, parent_skill)
-            self.data.set_user_skill(user, skill, user_skill)
-            return user_skill
-        else:
-            return user_skill
+                return user_skill, user_skill
+
+        # initialize skill if does not exist
+        if user_skill is None:
+            user_skill = 0
+
+        parent_user_skill, _ = self.get_user_skill(user, parent_skill)
+        return user_skill + parent_user_skill, user_skill
 
     def update(self):
         """
         update model using answer provided by data provider
         """
+        # get data
         answer = self.data.get_answer()
-
         user = self.data.get_user(answer)
         question = self.data.get_question(answer)
         question_type = self.data.get_question_type(question)
         leaf_skill = self.data.get_skill(question)
+        response = self.response(answer, question_type)
 
-        user_leaf_skill = self.get_user_skill(user, leaf_skill)
+        # get question difficulty
         difficulty = self.data.get_difficulty(question)
         if difficulty is None:
             difficulty = INITIAL_DIFFICULTY
             self.data.set_difficulty(question, difficulty)
 
-        expected_response = self.expected_response(
-            user_skill=user_leaf_skill,
-            difficulty=difficulty,
-            question_type=question_type
-        )
-        response = self.response(answer, question_type)
+        # update skills
+        level = 0   # level of updated skill
+        for skill in self.get_ancestor_skills(leaf_skill):
+            level += 1
+            user_skill, relative_user_skill = self.get_user_skill(user, skill)
+            expected_response = self.expected_response(
+                                user_skill=user_skill,
+                                difficulty=difficulty,
+                                question_type=question_type
+                            )
 
-        user_skill_delta = self.compute_user_skill_delta(response, expected_response, question_type)
-        self.data.set_user_skill(user, leaf_skill, user_leaf_skill + user_skill_delta)
-        self.update_parent_skills(user, self.data.get_parent_skill(leaf_skill), response, difficulty, question_type)
+            user_skill_delta = self.compute_user_skill_delta(response, expected_response, question_type, level)
+            self.data.set_user_skill(user, skill, relative_user_skill + user_skill_delta)
 
         if self.data.is_first_attempt(answer):
             difficulty_delta = self.compute_difficulty_delta(
@@ -103,18 +118,14 @@ class EloModel():
             )
             self.data.set_difficulty(question, difficulty + difficulty_delta)
 
-    def update_parent_skills(self, user, skill, response, difficulty, question_type):
-        if skill is None:
-            return
-        user_skill = self.get_user_skill(user, skill)
-        expected_response = self.expected_response(
-            user_skill=user_skill,
-            difficulty=difficulty,
-            question_type=question_type
-        )
-
-        user_skill_delta = self.compute_user_skill_delta(response, expected_response, question_type)
-        self.data.set_user_skill(user, skill, user_skill + user_skill_delta)
-
-
-        self.update_parent_skills(user, self.data.get_parent_skill(skill), response, difficulty, question_type)
+    def get_ancestor_skills(self, skill):
+        """
+        get all ancestor skills in tree (itself included)
+        skills are ordered from root to leaf
+        """
+        skills = []
+        tmp_skill = skill
+        while tmp_skill is not None:
+            skills = [tmp_skill] + skills
+            tmp_skill = self.data.get_parent_skill(tmp_skill)
+        return skills
--- model/views.py
+++ model/views.py
@@ -3,7 +3,7 @@ from lazysignup.decorators import allow_lazy_user
 import math
 from model.models import UserSkill, Skill
 
-
+user_skills_cache = {}
 @allow_lazy_user
 def my_skills(request):
     data = {}
@@ -12,6 +12,10 @@ def my_skills(request):
         skill = Skill.objects.get(name=name)
         data[skill] = getter(request.user)
         skills.append(skill)
+
+    user_skills_cache = {}
+
+    print skills
 
     return render(request, 'model/my_skills.html', {
         "data": data,
@@ -27,20 +31,47 @@ def get_user_skills(user, parent_list):
     for us in UserSkill.objects.filter(user=user.pk, skill__in=skills)\
             .select_related("skill"):
         user_skills[us.skill.name] = us
+        us.value = us.value + get_user_skill_value(user, us.skill.parent_id)   # compute skill from parents
         us.value_percent = int(100. / (1 + math.exp(-us.value)))
     return user_skills
 
 
-def get_user_skill(name, user):
-    skill = UserSkill.objects.filter(user=user, skill__name=name)
-    if len(skill) == 1:
-        skill = skill[0]
-        skill.value_percent = int(100. / (1 + math.exp(-skill.value)))
+def get_user_skill_by_name(name, user):
+    user_skill = UserSkill.objects.filter(user=user, skill__name=name)
+    if len(user_skill) == 1:
+        user_skill = user_skill[0]
+        user_skill.value = user_skill.value + get_user_skill_value(user, user_skill.skill.parent_id)   # compute skill from parents
+        user_skill.value_percent = int(100. / (1 + math.exp(-user_skill.value)))
     else:
-        skill = None
+        user_skill = None
 
-    return skill
+    return user_skill
 
+def get_user_skill_value(user, skill):
+    """
+    compute absolute skill of user
+    """
+    #look to the cache
+    # if "{0}-{1}".format(user.pk, skill) in user_skills_cache.keys():
+    #     return user_skills_cache["{0}-{1}".format(user.pk, skill)]
+
+    skill = Skill.objects.get(pk=skill)
+    user_skill = UserSkill.objects.get(user=user, skill=skill)
+
+    # stop recursion on root
+    if skill.parent is None:
+        if user_skill is None:
+            return 0
+        else:
+            return user_skill.value
+
+    # initialize skill if does not exist
+    if user_skill is None:
+        user_skill = 0
+
+    parent_user_skill_value = get_user_skill_value(user, skill.parent_id)
+    user_skills_cache["{0}-{1}".format(user.pk, skill.pk)] = user_skill.value + parent_user_skill_value
+    return user_skill.value + parent_user_skill_value
 
 def my_skills_numbers(user):
     user_skills = get_user_skills(user, ['numbers <= 10', 'numbers <= 20',
@@ -50,7 +81,7 @@ def my_skills_numbers(user):
         "table": [[get_skill_repr(str(c + r * 10), user_skills)
                    for c in range(1, 11)] for r in range(10)],
         "skills": get_user_skills(user, ["numbers"]),
-        "skill": get_user_skill("numbers", user),
+        "skill": get_user_skill_by_name("numbers", user),
     }
 
 
@@ -60,7 +91,7 @@ def my_skills_addition(user):
         "table": [[get_skill_repr('%s+%s' % (c, r), user_skills)
                    for c in range(1, 11)] for r in range(1, 21)],
         "skills": get_user_skills(user, ["addition"]),
-        "skill": get_user_skill("addition", user),
+        "skill": get_user_skill_by_name("addition", user),
     }
 
 
@@ -69,8 +100,8 @@ def my_skills_multiplication(user):
     return {
         "table": [[get_skill_repr('%sx%s' % (c, r), user_skills)
                    for c in range(11)] for r in range(21)],
-        "skills": get_user_skills(user, ["addition"]),
-        "skill": get_user_skill("multiplication", user),
+        "skills": get_user_skills(user, ["multiplication"]),
+        "skill": get_user_skill_by_name("multiplication", user),
     }
 
 
@@ -78,9 +109,9 @@ def my_skills_division(user):
     user_skills = get_user_skills(user, ['division1'])
     return {
         "table": [[get_skill_repr('%s/%s' % (a * b, b), user_skills)
-                   for a in range(11)] for b in range(1, 11)],
+             for a in range(11)] for b in range(1, 11)],
         "skills": get_user_skills(user, ["division"]),
-        "skill": get_user_skill("division", user),
+        "skill": get_user_skill_by_name("division", user),
     }
 
 
