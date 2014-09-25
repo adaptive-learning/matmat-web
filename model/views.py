ed0af94c9836724d61e41e13b84f2016b6408a6f
Juraj Niznan <jurajniznan@gmail.com>
2014-09-25 16:20:18 +0200

    my skills change refactor

--- model/views.py
+++ model/views.py
@@ -1,175 +1,158 @@
-import colorsys
+from collections import defaultdict
+from django.db import connection
 from django.shortcuts import render
 from lazysignup.decorators import allow_lazy_user
+import colorsys
 import math
-from model.models import UserSkill, Skill
+from collections import namedtuple
 
 
 NAMES = ('numbers', 'addition', 'subtraction', 'multiplication', 'division')
+SKILL_TABLES = {
+    'numbers':
+    [[str(c + r * 10) for c in range(1, 11)] for r in range(2)],
+    'addition':
+    [['%s+%s' % (c, r) for c in range(1, 11)] for r in range(1, 21)],
+    'subtraction':
+    [['%s-%s' % (r, c) for c in range(1, r + 1)] for r in range(1, 21)],
+    'multiplication':
+    [['%sx%s' % (c, r) for c in range(11)] for r in range(21)],
+    'division':
+    [['%s/%s' % (a * b, b) for a in range(11)] for b in range(1, 11)],
+}
+
+skill_keys = ['pk', 'name', 'note', 'value', 'value_percent', 'style']
+SkillTuple = namedtuple('Skill', ', '.join(skill_keys))
+skill_as_tuple = lambda skill: SkillTuple(*[skill[k] for k in skill_keys])
 
 
 @allow_lazy_user
 def my_skills(request, pk=None):
-    get_user_skill_value.clear()    # clear cache for skill value getter
+    all_skills = get_all_skills(request.user)
 
-    active = "numbers"
+    par = None
+    while pk is not None:
+        par, pk = pk, all_skills['id_parid'][pk]
+    active = all_skills['id_name'][par] if par is not None else 'numbers'
 
-    data = {}
+    data = []
     skills = []
     for name in NAMES:
-        skill = Skill.objects.get(name=name)
-        getter = globals()['my_skills_' + name]
-        data[skill] = getter(request.user)
-        skill.style = get_style(data[skill]["skill"])
+        skill = get_skill_obj_by_name(name, all_skills)
         skills.append(skill)
-        if pk in skill.children_list.split(","):
-            active = name
+        data.append(get_my_skills(name, all_skills))
 
-    return render(request, 'model/my_skills.html', {
+    ret = render(request, 'model/my_skills.html', {
         "data": data,
         "skills": skills,
         "active": active,
     })
 
-
-def get_user_skills(user, parent_list):
-    skills = Skill.objects.filter(parent__name__in=parent_list)
-    skills_name = set([s.name for s in skills])
-    user_skills = {k: None for k in skills_name}
-    for us in UserSkill.objects.filter(user=user.pk, skill__in=skills)\
-            .select_related("skill"):
-        user_skills[us.skill.name] = us
-        # compute skill from parents:
-        us.value = us.value + get_user_skill_value(user, us.skill.parent_id)
-        us.value_percent = int(100. / (1 + math.exp(-us.value)))
-        us.style = get_style(us)
-    return user_skills
+    return ret
 
 
-def get_user_skill_by_name(name, user):
-    user_skill = UserSkill.objects.filter(user=user, skill__name=name)
-    if len(user_skill) == 1:
-        user_skill = user_skill[0]
-        # compute skill from parents:
-        user_skill.value = user_skill.value +\
-            get_user_skill_value(user, user_skill.skill.parent_id)
-        user_skill.value_percent = \
-            int(100. / (1 + math.exp(-user_skill.value)))
-        user_skill.style = get_style(user_skill)
-    else:
-        user_skill = None
+def get_all_skills(user):
+    cursor = connection.cursor()
+    cursor.execute(
+        '''SELECT s.id, s.parent_id, s.name, usk.val, s.note
+        FROM model_skill s LEFT JOIN
+            (SELECT us.skill_id as sid, us.value as val
+             FROM model_userskill us
+             WHERE us.user_id={0}) usk
+        ON (s.id = usk.sid)
+        '''.format(user.pk))
+    rows = cursor.fetchall()
+    skills = {sid: (parid, name, val, note)
+              for sid, parid, name, val, note in rows}
+    user_skills = {}
+    name_id = {}
+    id_name = {}
+    id_parid = {}
+    id_note = {}
+    id_used = {}
+    children = defaultdict(list)
+    for sid, (parid, name, val, note) in skills.iteritems():
+        get_skill_value(sid, skills, user_skills)
+        name_id[name] = sid
+        id_name[sid] = name
+        id_parid[sid] = parid
+        id_note[sid] = note
+        id_used[sid] = val is not None
+        if parid:
+            children[parid].append(sid)
+    return {'name_id': name_id,
+            'id_name': id_name,
+            'id_val': user_skills,
+            'id_parid': id_parid,
+            'children': children,
+            'id_note': id_note,
+            'id_used': id_used,
+            }
 
-    return user_skill
+
+def get_children(parent_list, skills):
+    parids = [skills['name_id'][p] for p in parent_list]
+    children = [ch for parid in parids for ch in skills['children'][parid]]
+    ret = {}
+    for sid in children:
+        obj = get_skill_obj(sid, skills)
+        ret[obj.name] = obj
+    return ret
 
 
-def memoize(f):
-    """ Memoization decorator for functions taking one or more arguments. """
-    class memodict(dict):
-        def __init__(self, f):
-            self.f = f
-
-        def __call__(self, *args):
-            return self[args]
-
-        def __missing__(self, key):
-            ret = self[key] = self.f(*key)
-            return ret
-
-    return memodict(f)
+def get_skill_obj(sid, skills):
+    obj = {'name': skills['id_name'][sid],
+           'value': skills['id_val'][sid],
+           'pk': sid,
+           'note': skills['id_note'][sid]}
+    obj['value_percent'] = int(100. / (1 + math.exp(-obj['value'])))
+    obj['style'] = get_style(obj['value'], used=skills['id_used'][sid])
+    return skill_as_tuple(obj)
 
 
-@memoize
-def get_user_skill_value(user, skill):
+def get_skill_value(sid, skills, user_skills):
     """
-    compute absolute skill of user
+    compute and save absolute skill of user
     """
-
-    skill = Skill.objects.get(pk=skill)
-    user_skill = UserSkill.objects.get(user=user, skill=skill)
-
-    # stop recursion on root
-    if skill.parent is None:
-        if user_skill is None:
-            return 0
-        else:
-            return user_skill.value
-
-    # initialize skill if does not exist
-    if user_skill is None:
-        user_skill = 0
-
-    parent_user_skill_value = get_user_skill_value(user, skill.parent_id)
-    return user_skill.value + parent_user_skill_value
+    if sid not in user_skills:
+        parid, name, val, _ = skills[sid]
+        val = val or 0
+        parval = get_skill_value(parid, skills, user_skills) if parid else 0
+        user_skills[sid] = val + parval
+    return user_skills[sid]
 
 
-def my_skills_numbers(user):
-    user_skills = get_user_skills(user, ['numbers <= 10', 'numbers <= 20',
-                                         'numbers <= 100'])
+def get_skill_obj_by_name(name, skills):
+    sid = skills['name_id'][name]
+    return get_skill_obj(sid, skills)
 
+
+def get_my_skills(name, skills):
+    top = get_children([name], skills)
+    children = get_children([s.name for s in top.values()], skills)
     return {
-        "table": [[get_skill_repr(str(c + r * 10), user_skills)
-                   for c in range(1, 11)] for r in range(2)],
-        "skills": get_user_skills(user, ["numbers"]),
-        "skill": get_user_skill_by_name("numbers", user),
-    }
-
-
-def my_skills_addition(user):
-    user_skills = get_user_skills(user, ['addition <= 10', 'addition <= 20'])
-    return {
-        "table": [[get_skill_repr('%s+%s' % (c, r), user_skills)
-                   for c in range(1, 11)] for r in range(1, 21)],
-        "skills": get_user_skills(user, ["addition"]),
-        "skill": get_user_skill_by_name("addition", user),
-    }
-
-
-def my_skills_subtraction(user):
-    user_skills = get_user_skills(user, ['subtraction <= 10',
-                                         'subtraction <= 20'])
-    return {
-        "table": [[get_skill_repr('%s-%s' % (r, c), user_skills)
-                   for c in range(1, r + 1)] for r in range(1, 21)],
-        "skills": get_user_skills(user, ["subtraction"]),
-        "skill": get_user_skill_by_name("subtraction", user),
-    }
-
-
-def my_skills_multiplication(user):
-    user_skills = get_user_skills(user, ['multiplication1', 'multiplication2'])
-    return {
-        "table": [[get_skill_repr('%sx%s' % (c, r), user_skills)
-                   for c in range(11)] for r in range(21)],
-        "skills": get_user_skills(user, ["multiplication"]),
-        "skill": get_user_skill_by_name("multiplication", user),
-    }
-
-
-def my_skills_division(user):
-    user_skills = get_user_skills(user, ['division1'])
-    return {
-        "table": [[get_skill_repr('%s/%s' % (a * b, b), user_skills)
-                  for a in range(11)] for b in range(1, 11)],
-        "skills": get_user_skills(user, ["division"]),
-        "skill": get_user_skill_by_name("division", user),
+        "table": [[get_skill_repr(c, children) for c in r]
+                  for r in SKILL_TABLES[name]],
+        "skills": top,
+        "skill": get_skill_obj_by_name(name, skills),
     }
 
 
 def get_skill_repr(name, user_skills):
     if name in user_skills:
-        return {'name': name, 'style': get_style(user_skills[name])}
+        return {'name': name, 'style': user_skills[name].style}
     else:
-        return {'name': '', 'style': get_style(None, name)}
+        return {'name': '', 'style': get_style(None)}
 
 
-def get_style(user_skill, name=None):
+def get_style(value, used=True):
     ''' return css style of user skill'''
-    if user_skill is None:
-        if name is None:
-            return 'background-color: rgba(127, 127, 0, 0.2);'
+    if value is None:
         return 'background-color: rgba(127, 127, 0, 0);'
 
-    value = (1 / (1 + math.exp(-user_skill.value)))
-    color = colorsys.hsv_to_rgb(1./12 + value * 2 / 9., 1, 0.8)
-    color = [int(c*255) for c in color]
-    return "background-color: rgba({0[0]}, {0[1]}, {0[2]}, 1);".format(color)
+    value = (1 / (1 + math.exp(-value)))
+    color = colorsys.hsv_to_rgb(1. / 12 + value * 2 / 9., 1, 0.8)
+    color = [int(c * 255) for c in color]
+    alpha = 1 if used else 0.2
+    return "background-color: rgba({0[0]}, {0[1]}, {0[2]}, {1});".\
+        format(color, alpha)
