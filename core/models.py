b9ea898e6f5909aa9b9287ae5e9638f4a5aa2aa0
Thran <exthran@gmail.com>
2014-09-25 16:29:58 +0200

    add profile for users

--- core/models.py
+++ core/models.py
@@ -1,3 +1,18 @@
+from django.contrib.auth.models import User
 from django.db import models
+from django.db.models.signals import post_save
+from django.dispatch import receiver
 
-# Create your models here.
+
+class UserProfile(models.Model):
+    user = models.OneToOneField(User)
+    children = models.ManyToManyField('self', related_name="supervisors")
+
+
+@receiver(post_save, sender=User)
+def create_profile(sender, **kwargs):
+    instance = kwargs["instance"]
+    created = kwargs["created"]
+    if created:
+        profile = UserProfile(user=instance)
+        profile.save()
