361f3743355720de3db187cd9395ec8e0cf8d581
Thran <exthran@gmail.com>
2015-02-15 23:22:28 +0100

    nicer solution for roller rolling

--- static/simulators/roller-wizard.js
+++ static/simulators/roller-wizard.js
@@ -10,7 +10,10 @@ app.directive("rollerwizard", function($timeout){
             $scope.global = SimulatorGlobal;
 
             $scope.prepare_height = function(){
-                $('#roller-wizard').find("#roller-playground").css("height", $("#roller-wizard").find("#roller-playground > div").height())
+                console.log($("#roller-wizard").find("#roller-playground > div").height());
+                $timeout( function() {
+                    $('#roller-wizard').find("#roller-playground").css("height", $("#roller-wizard").find("#roller-playground > div").height())
+                }, 100);
             };
 
             $scope.$watch("closed", function(o, n){
