88b926b47dd652ec4a81a77864771d4bd1bf280a
Thran <exthran@gmail.com>
2015-03-02 15:05:29 +0100

    new graphics fo my stats

--- static/scripts.js
+++ static/scripts.js
@@ -3,8 +3,8 @@ generate_cubes = function (count, canvas_id) {
     for (var i=0; i < count; i++){
         var cube = $("<div></div>");
         cube.css("background-color", random_color());
-        cube.css("left", Math.random()*100 - 3 + "%");
-        cube.css("top", Math.random()*100 - 3 + "%");
+        cube.css("left", Math.random()*100 - 15 + "%");
+        cube.css("top", Math.random()*100 - 15 + "%");
         cube.css("transform", "rotate({0}deg)".format(Math.random() * 90));
         canvas.append(cube);
     }
