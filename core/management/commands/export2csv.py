629750d7a1967e670406b8c44f2b5c289e837e7d
Thran <exthran@gmail.com>
2015-10-19 13:41:06 +0200

    processing data to expert

--- core/management/commands/export2csv.py
+++ core/management/commands/export2csv.py
@@ -1,3 +1,4 @@
+from collections import defaultdict
 from contextlib import closing
 import csv
 import zipfile
@@ -8,11 +9,13 @@ import re
 from matmat import settings
 from questions.models import Answer, Question, Simulator
 from model.models import Skill
+import pandas as pd
+import json
 
 
 class Command(BaseCommand):
     args = 'table name'
-    help = "Delete all lazy user without data"
+    help = "Export data"
 
     BATCH_SIZE = 5 * 10**5
     MODELS_TO_EXPORT = [Question, Answer, Simulator, Skill]
@@ -39,20 +42,20 @@ class Command(BaseCommand):
             self.handle_all_tables()
 
     def handle_all_tables(self):
+        if not os.path.exists(os.path.join(settings.MEDIA_ROOT, "raw")):
+            os.makedirs(os.path.join(settings.MEDIA_ROOT, "raw"))
         for table_name in self.tables_to_export:
             self.handle_one_table(table_name)
 
-        filename_zip = settings.MEDIA_ROOT + '/' + "matmat_export.zip"
+        prepare_data(input_dir=os.path.join(settings.MEDIA_ROOT, "raw"), output_dir=settings.MEDIA_ROOT)
+        filename_zip = os.path.join(settings.MEDIA_ROOT, "matmat_export_raw.zip")
+        files = [os.path.join(settings.MEDIA_ROOT, "raw", f + '.csv') for f in self.tables_to_export]
+        zip_files(filename_zip, files)
 
-        if os.path.exists(filename_zip):
-            os.remove(filename_zip)
+        filename_zip = os.path.join(settings.MEDIA_ROOT, "matmat_export.zip")
+        files = [os.path.join(settings.MEDIA_ROOT, f + '.csv') for f in ["answers", "items", "skills"]]
+        zip_files(filename_zip, files)
 
-        zf = zipfile.ZipFile(filename_zip, 'w', zipfile.ZIP_DEFLATED)
-        for table_name in self.tables_to_export:
-            filename_csv = settings.MEDIA_ROOT + '/' + table_name + '.csv'
-            zf.write(filename_csv, os.path.basename(filename_csv))
-            os.remove(filename_csv)
-        zf.close()
 
     def handle_one_table(self, table_name):
         if table_name not in self.tables_to_export:
@@ -65,7 +68,7 @@ class Command(BaseCommand):
         print 'processing %s' % table_name, ',', count, 'items'
 
         sql = 'SELECT * FROM ' + table_name
-        filename_csv = settings.MEDIA_ROOT + '/' + table_name + '.csv'
+        filename_csv = settings.MEDIA_ROOT + '/raw/' + table_name + '.csv'
         for offset in xrange(0, count, self.BATCH_SIZE):
             with closing(connection.cursor()) as cursor:
                 cursor.execute(sql + ' LIMIT ' + str(self.BATCH_SIZE) + ' OFFSET ' + str(offset))
@@ -83,6 +86,74 @@ class Command(BaseCommand):
             for row in cursor:
                 row = [val.encode('utf-8') if isinstance(val, unicode) else val for val in row]
                 writer.writerow(row)
+
+def zip_files(filename_zip, files):
+    if os.path.exists(filename_zip):
+        os.remove(filename_zip)
+
+    zf = zipfile.ZipFile(filename_zip, 'w', zipfile.ZIP_DEFLATED)
+    for filename in files:
+        zf.write(filename, os.path.basename(filename))
+        # os.remove(filename)
+    zf.close()
+
+def get_skill_parents(skills):
+    map = {}
+    for id, skill in skills.iterrows():
+        map[id] = int(skill["parent"]) if not pd.isnull(skill["parent"]) else None
+    return map
+
+
+def get_skill_parent_lists(skills):
+    map = get_skill_parents(skills)
+    lists = defaultdict(lambda: [])
+    for skill in map:
+        s = skill
+        while s:
+            lists[skill].append(s)
+            s = map[s]
+    return lists
+
+
+def prepare_data(input_dir="data/source", output_dir="data"):
+    answers = pd.DataFrame.from_csv(os.path.join(input_dir, "questions_answer.csv"))
+    items = pd.DataFrame.from_csv(os.path.join(input_dir, "questions_question.csv"))
+    simulators = pd.DataFrame.from_csv(os.path.join(input_dir, "questions_simulator.csv"))
+    skills = pd.DataFrame.from_csv(os.path.join(input_dir, "model_skill.csv"))
+    skill_parents = get_skill_parent_lists(skills)
+
+    items = items.join(simulators, "player")
+    items.rename(inplace=True, columns={"name": "visualization"})
+    items["answer"] = 0
+    items["skill_lvl_1"], items["skill_lvl_2"], items["skill_lvl_3"] = None, None, None
+    for id, item in items.iterrows():
+        data = json.loads(item["data"])
+        items.loc[id, "data"] = item["data"].replace('"', "'")
+        items.loc[id, "answer"] = int(data["answer"])
+        for i, skill in enumerate(skill_parents[item["skill"]][::-1][1:]):
+            items.loc[id, "skill_lvl_{}".format(i + 1)] = skill
+    items = items[["answer", "visualization", "skill", "skill_lvl_1", "skill_lvl_2", "skill_lvl_3", "data"]]
+
+    answers.rename(inplace=True, columns={
+        "question": "item",
+        "user": "student",
+        "solving_time": "response_time",
+        "correctly_solved": "correct",
+    })
+    answers = answers.join(items[["answer"]], on="item", rsuffix="_expected")
+    answers = answers[["timestamp", "item", "student", "response_time", "correct", "answer", "answer_expected", "device", "log"]]
+    answers = answers.round({"response_time": 3})
+
+    skills.rename(inplace=True, columns={"note": "name_cz",})
+    skills = skills[["name", "parent"]]
+
+    simulators.rename(inplace=True, columns={"note": "name_cz",})
+
+    answers.to_csv(os.path.join(output_dir, "answers.csv"))
+    items.to_csv(os.path.join(output_dir, "items.csv"))
+    # simulators.to_csv(os.path.join(output_dir, "visualizations.csv"))
+    skills.to_csv(os.path.join(output_dir, "skills.csv"), float_format="%.0f")
+
 
 
 
