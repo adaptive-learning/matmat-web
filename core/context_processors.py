c88cc1067ba56698d8784764cafa32596b323f7d
Thran <exthran@gmail.com>
2015-12-22 17:23:08 +0100

    add meta data for FB loading image
    add meta data for FB loading image

--- core/context_processors.py
+++ core/context_processors.py
@@ -6,4 +6,5 @@ def important(request):
     return {
         'GOOGLE_ANALYTICS': settings.ON_VIPER and not settings.DEVEL,
         'graphics': request.user.profile.graphics if is_user_registred(request.user) else settings.DEFAULT_GRAPHICS,
+        'DOMAIN': request.build_absolute_uri('/')[:-1],
     }
