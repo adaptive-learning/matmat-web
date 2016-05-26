0ce45e02a7e4f7988fd173ccc64c0820d7c09fa4
Thran <exthran@gmail.com>
2016-05-26 16:29:16 +0200

    answer migrations

--- README.rst
+++ README.rst
@@ -8,4 +8,7 @@ Migration from old version
 ==========================
 
 python manage.py deletelazyusers (on old system)
-python manage.py migrate_data flush users profiles
+python manage.py migrate_data flush
+python manage.py migrate_data users profiles
+python manage.py migrate_data questions
+python manage.py migrate_data answers
--- matmat/management/commands/migrate_data.py
+++ matmat/management/commands/migrate_data.py
@@ -1,11 +1,18 @@
 import json
-
+from collections import defaultdict
 import os
+from clint.textui import progress
+from datetime import timedelta
 from django.contrib.auth.models import User
 from django.core.management import call_command
 from django.core.management.base import BaseCommand
 from django.db import connections
-from proso_user.models import UserProfile
+from django.db.models.signals import post_save
+from proso.django.config import get_global_config
+from proso_common.models import Config
+from proso_models.models import update_predictive_model, AnswerMeta
+from proso_tasks.models import TaskInstance, TaskAnswer
+from proso_user.models import UserProfile, Session
 
 
 class Command(BaseCommand):
@@ -50,7 +57,7 @@ class Command(BaseCommand):
                            '    ON u.id = c.from_userprofile_id'
                            '  LEFT JOIN core_userprofile cp '
                            '    ON c.to_userprofile_id = cp.id')
-            for profile in dictfetchall(cursor):
+            for profile in dict_fetch_all(cursor):
                 if profile['user_id'] in profiles:
                     profiles[profile['user_id']]['children'].append(profile['child'])
                 else:
@@ -89,13 +96,77 @@ class Command(BaseCommand):
         call_command('loaddata', file_name)
 
     def migrate_answers(self):
-        pass
+        post_save.disconnect(update_predictive_model)
+
+        Session.objects.all().delete()
+        TaskAnswer.objects.all().delete()
+
+        old_new_id_map = json.load(open('matmat/management/commands/old_new_id_map.json'))
+        old_new_id_map = {k: v for k, v in old_new_id_map}
+        task_instances = {t.identifier: t for t in TaskInstance.objects.all().select_related('task')}
+        config = Config.objects.from_content(get_global_config()).id
+
+        with connections['old'].cursor() as cursor:
+            cursor.execute('SELECT '
+                           '    a.timestamp, '
+                           '    a.user_id, '
+                           '    a.correctly_solved, '
+                           '    a.solving_time, '
+                           '    a.answer,'
+                           '    a.device, '
+                           '    q.identifier'
+                           ' FROM questions_answer a '
+                           ' LEFT JOIN questions_question q '
+                           '    ON q.id = a.question_id '
+                           ' WHERE q.active IS TRUE '
+                           ' ORDER BY a.id'
+                           )
+
+            sessions = defaultdict(lambda: (None, None))
+            metas = {}
+            answers = []
+            for answer in progress.bar(dict_fetch_all(cursor),
+                                       every=max(1, min(100, cursor.rowcount // 1000)), expected_size=cursor.rowcount):
+                user = answer['user_id']
+
+                if answer['identifier'] not in old_new_id_map:
+                    print('Question ID not found: ', answer['identifier'])
+
+                task_instance = task_instances[old_new_id_map[answer['identifier']]]
+
+                session, last_update = sessions[user]
+                if session is None or (last_update is not None and (answer['timestamp'] - last_update) > timedelta(minutes=30)):
+                    session = Session.objects.create(user_id=user)
+                sessions[user] = session, answer['timestamp']
+
+                device = answer['device']
+                if device in metas:
+                    meta = metas[device]
+                else:
+                    meta = AnswerMeta.objects.from_content({'device': device})
+                    metas[device] = meta
+
+                a = TaskAnswer(
+                    user_id=user,
+                    item_id=task_instance.item_id,
+                    item_asked_id=task_instance.item_id,
+                    item_answered_id=task_instance.item_id if answer['correctly_solved'] else None,
+                    response_time=answer['solving_time'] * 1000,
+                    lang='cs',
+                    question=task_instance.task.identifier,
+                    answer=answer['answer'],
+                    session_id=session.pk,
+                    config_id=config,
+                    metainfo_id=meta.pk
+                )
+                answers.append(a)
+                a.save()
 
 
-def dictfetchall(cursor):
+def dict_fetch_all(cursor):
     "Return all rows from a cursor as a dict"
     columns = [col[0] for col in cursor.description]
     return [
         dict(zip(columns, row))
         for row in cursor.fetchall()
-        ]
+    ]
