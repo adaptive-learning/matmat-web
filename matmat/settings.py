65c5ffe7bcb4c0f80c33d0ce5e0344b076f5e08c
Jan Papousek <jan.papousek@gmail.com>
2016-08-09 11:47:49 +0200

    use own memcached instance on production server

--- matmat/settings.py
+++ matmat/settings.py
@@ -92,7 +92,7 @@ if ON_SERVER and not ON_DEV:
     CACHES = {
         'default': {
             'BACKEND': 'django.core.cache.backends.memcached.MemcachedCache',
-            'LOCATION': '127.0.0.1:11211',
+            'LOCATION': '127.0.0.1:11212',
         }
     }
 
